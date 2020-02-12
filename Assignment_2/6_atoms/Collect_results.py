5cbf76bea18e31b9be9aae6e6070412f0e0267f6
Hanna Hallborn <hallborn@hebbe2.int.private>
2020-02-12 10:27:29 +0100

    -

--- Assignment_2/6_atoms/Collect_results.py
+++ Assignment_2/6_atoms/Collect_results.py
@@ -10,6 +10,20 @@ from ase.io import write
 traj = Trajectory('all_candidates.traj') 	# traj is sorted by energy
 atoms = traj[0] 				# Atom configuration with lowest energy
 
-view(atoms)
+#view(atoms)
 
 # write('least_energy_traj_Na6.xyz', atoms)       # Write atoms object to .xyz file
+
+########################################################
+from ase.db import connect
+db = connect('gadb.db')
+
+atoms2 = db.select(sort='energy')  # atoms2 is a generator object
+
+atoms2_sorted_list=[]
+
+for atom in atoms2:
+        atoms2_sorted_list.append(atom.toatoms())
+
+
+#print(atoms2_sorted_list)
