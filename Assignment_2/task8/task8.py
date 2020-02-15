b6e9ae3ed108fdf6f008000a316240d06cd6daf6
Hanna Hallborn <hallborn@hebbe2.int.private>
2020-02-15 14:32:08 +0100

    -

--- Assignment_2/task8/task8.py
+++ Assignment_2/task8/task8.py
@@ -2,6 +2,7 @@
 from ase import Atom
 from ase import Atoms
 from gpaw import GPAW, PW
+from ase.optimize import BFGS
 from ase.visualize import view
 #from ase.io.trajectory import Trajectory
 #from ase.io import write
@@ -13,46 +14,61 @@ from ase.db import connect
 Na6_lowest = read('christmas-tree.xyz')
 Na6_2nd_lowest = read('half-decahedron.xyz')
 
+E1 = Na6_2nd_lowest.get_potential_energy()
+print(E1)
+
+"""
 # Connect to database
 db = connect('t8db.db')
 
 
-def relax(Cluster_to_relax, mod, name):
+def relax(Cluster_to_relax, mod, exc, name):
     # Cluster_to_relax : Atoms object to be optimized
-    # mod : GPAW mode parameter e.g. 'lcao', PW()
+    # mod : GPAW mode parameter e.g. 'lcao', PW(), fd
+    # exc : exchange correlation term
     # name : string with name of txt file and qpw file
     name_of_txt_file = name + '.txt'
     name_of_gpw_file = name + '.gpw'
     
     # Initialize calculator
-    calc = GPAW(mode=mod, txt=name_of_txt_file)  	
+    calc = GPAW(mode=mod, xc=exc, txt=name_of_txt_file)  	
     Cluster_to_relax.set_calculator(calc)
-    
-    E = Cluster_to_relax.get_potential_energy()
+    dyn = BFGS(Cluster_to_relax)
+    dyn.run(fmax=0.02)
+    Cluster_to_relax.get_potential_energy()
     #calc.write(name_of_gpw_file)
+"""
 
-
+"""
 # define parameters
-mode_of_GPAW = PW()
-run_nr = 2
+mode_of_GPAW = 'lcao'
+mode_str = 'lcao'
+exchange = 'LDA'
+run_nr = 3
 
 # relax structures
-relax(Na6_lowest, mode_of_GPAW, 'struc1_run'+str(run_nr))
-relax(Na6_2nd_lowest, mode_of_GPAW, 'struc2_run'+str(run_nr))
+relax(Na6_lowest, mode_of_GPAW, exchange, 'struc1_run'+str(run_nr))
+relax(Na6_2nd_lowest, mode_of_GPAW, exchange, 'struc2_run'+str(run_nr))
 
 
 # write result to database
-db.write(Na6_lowest, structure=1, GPAWmode=str(mode_of_GPAW), run=run_nr)
-db.write(Na6_2nd_lowest, structure=2, GPAWmode=str(mode_of_GPAW), run=run_nr)
+db.write(Na6_lowest, structure=1, GPAWmode=mode_str, xc=exchange, run=run_nr)
+db.write(Na6_2nd_lowest, structure=2, GPAWmode=mode_str, xc=exchange, run=run_nr)
 
+"""
 
 """
 run 1:
-mode = 'lcao'
+mode='lcao'
+xc='PBE'
 
 run 2:
-mode = PW()
+mode=PW(350)
+xc='PBE'
 
+run 3:
+mode='lcao'
+xc='LDA'
 """
 
 
