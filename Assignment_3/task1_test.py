c2297e0f9e2cab444f213c76ef2cdcb4f9ae3638
Hanna Student <hallborn@student.chalmers.se>
2020-02-19 21:26:23 +0100

    -

---
+++ Assignment_3/task1_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from ase import Atom
+from ase import Atoms
+#from gpaw import GPAW, PW
+from ase.visualize import view
+from ase.io.trajectory import Trajectory
+from ase.io import read
+
+import numpy as np
+import matplotlib.pyplot as plt
+#%%
+traj = Trajectory('cluster24.traj')  # 6 ps AIMD simulation of 24 water molecules
+
+
+log = open('cluster24.log', 'r')
+t, E_tot, E_pot, E_kin, T = np.loadtxt(log, skiprows=1, unpack=True)
+
+
+mean_T = sum(T)/len(T)
+#%%
+fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+ax[0].plot(t, T)
+ax[0].plot([0, 6],[mean_T, mean_T])
+ax[0].set_xlabel('time [ps]', fontsize=15)
+ax[0].set_ylabel('temperature [K]', fontsize=15)
+
+t_max = 3000
+ax[1].plot(t[0:t_max], T[0:t_max])
+ax[1].plot([0, 1.5],[mean_T, mean_T])
+ax[1].set_xlabel('time [ps]', fontsize=15)
+ax[1].set_ylabel('temperature [K]', fontsize=15)
+#fig.savefig('temp_vs_time.png')
+
+timestep = t[1]-t[0]   # timestep used in the AIMD simulation
+n = int(1/timestep)
+
+#%%
+config_nr = n               # n+7 has temp 347.5 (T_mean = 345.1)
+
+atoms = traj[config_nr]
+print(T[config_nr])
+view(atoms)
+    
+Na_ion = Atom('Na', charge=1)
